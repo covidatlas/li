3ca8760b281cf4e94be383929d0de1da5dccefd3
Ryan Block <rblock@gmail.com>
2020-04-14 16:37:43 -0700

    Make cli a bit nicer

---
+++ src/cli/make-nice.js
@@ -0,0 +1,30 @@
+const { sep } = require('path')
+const loadSources = require('../../src/shared/sources/_lib/load-sources.js')
+
+/**
+ * Enable:
+ * --[source|crawl|scrape] 'nl' instead of 'nl/index.js'
+ * --[source|crawl|scrape] 'us/ca/san-francisco-county' instead of 'us/ca/san-francisco-county.js'
+ */
+module.exports = function makeNice (params) {
+  let { source, crawl, scrape } = params
+
+  if (crawl || scrape || source) {
+
+    const sources = loadSources().map(s => s.split(`shared${sep}sources`)[1].substr(1))
+    const makeNice = p => {
+      const exact = sources.find(s => p === s)
+      if (exact) return exact
+
+      const sansJs = sources.find(s => p === s.replace('.js', ''))
+      if (sansJs) return sansJs
+
+      const sansIndexJs = sources.find(s => p === s.replace(`${sep}index.js`, ''))
+      if (sansIndexJs) return sansIndexJs
+    }
+    if (source) source = makeNice(source)
+    if (crawl) crawl = makeNice(crawl)
+    if (scrape) scrape = makeNice(scrape)
+  }
+  return { source, crawl, scrape }
+}
