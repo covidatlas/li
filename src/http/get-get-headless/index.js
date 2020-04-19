dce9d84999c1ad9eaf4cf25ffaf3a46bc383bbe1
Ryan Block <rblock@gmail.com>
2020-04-18 21:31:35 -0700

    Clean up error handling in get-headless endpoint

--- src/http/get-get-headless/index.js
+++ src/http/get-get-headless/index.js
@@ -38,74 +38,46 @@ async function getHeadless (req) {
       await page.setExtraHTTPHeaders({ cookie })
     }
 
-    let tries = 0
-    while (tries < 5) {
-      tries++
-      if (tries > 1) {
-        await new Promise(r => setTimeout(r, 2000))
-      }
+    const response = await page.goto(url, {
+      timeout,
+      waitUntil: 'networkidle2'
+    })
 
-      const response = await page.goto(url, {
-        timeout,
-        waitUntil: 'networkidle2'
-      })
+    const status = response && response.status()
+    const ok = `${status}`.startsWith(2)
 
-      // Some sort of internal socket error or other badness, retry
-      if (response === null) {
-        browser.close()
-        continue
-      }
+    // We got a good response, return it
+    if (ok) {
+      await page.waitFor(responseTimeout)
+      const html = await page.content()
+      browser.close()
 
-      const status = response.status()
-
-      // Try again if we got an error code which might be recoverable
-      if (status >= 500) {
-        continue
-      }
-
-      // We got a good response, return it
-      if (status < 400) {
-        await page.waitFor(responseTimeout)
-        const html = await page.content()
-        browser.close()
-
-        // Compress, then base64 encode body in case we transit binaries, max 10MB payload
-        let responseBody = new Buffer.from(html)
-        responseBody = brotliCompressSync(responseBody).toString('base64')
-        if (responseBody.length >= 1000 * 1000 * 10) {
-          console.log(`Hit a very large payload!`, JSON.stringify(options, null, 2))
-          return {
-            statusCode: 500,
-            json: { error: 'maximum_size_exceeded' }
-          }
-        }
-        // Set up response payload
-        let payload = {
-          body: responseBody
-        }
-        // TODO handle cookies, haven't seen a need yet
-        // → page.cookies
-
-        const body = JSON.stringify(payload)
+      // Compress, then base64 encode body in case we transit binaries, max 10MB payload
+      let responseBody = new Buffer.from(html)
+      responseBody = brotliCompressSync(responseBody).toString('base64')
+      if (responseBody.length >= 1000 * 1000 * 10) {
+        console.log(`Hit a very large payload!`, JSON.stringify(options, null, 2))
         return {
-          statusCode: 200,
-          body
+          statusCode: 500,
+          json: { error: 'maximum_size_exceeded' }
         }
       }
-
-      // 400-499 means retrying is not likely to help
-      if (status < 500) {
-        browser.close()
-        return {
-          statusCode: status
-        }
+      // Set up response payload
+      let payload = {
+        body: responseBody
       }
+      // TODO handle cookies, haven't seen a need yet
+      // → page.cookies
 
-      if (tries === 5) {
-        browser.close
-        return {
-          statusCode: 500
-        }
+      const body = JSON.stringify(payload)
+      return {
+        statusCode: 200,
+        body
+      }
+    }
+    else {
+      return {
+        statusCode: status || 599 // Kinda? But 599 it's weird and noticeable
       }
     }
   }
@@ -113,9 +85,11 @@ async function getHeadless (req) {
     if (browser) {
       browser.close()
     }
-    console.error('An error occurred', err)
+    console.error('Some manner of strange error occurred:', err)
+    console.log('Params:', JSON.stringify(options, null, 2))
     return {
-      statusCode: 500
+      statusCode: 500,
+      json: { error: err.stack }
     }
   }
 }
