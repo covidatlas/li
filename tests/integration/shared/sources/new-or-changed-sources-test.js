674c11df076f953001c5c70c90c820d59e09c150
Jeff Zohrab <jzohrab@gmail.com>
2020-04-21 15:44:03 -0600

    new-or-changed works on its own

--- tests/integration/shared/sources/new-or-changed-sources-test.js
+++ tests/integration/shared/sources/new-or-changed-sources-test.js
@@ -5,7 +5,7 @@ const test = require('tape')
 const srcShared = path.join(process.cwd(), 'src', 'shared')
 const datetime = require(path.join(srcShared, 'datetime', 'index.js'))
 const sourceMap = require(path.join(srcShared, 'sources', '_lib', 'source-map.js'))
-const changedSources = require('./_lib/changed-sources.js')
+const changedSources = require(path.join(__dirname, '_lib', 'changed-sources.js'))
 
 const srcEvents = path.join(process.cwd(), 'src', 'events')
 const crawlerHandler = require(path.join(srcEvents, 'crawler', 'index.js')).handler
@@ -49,74 +49,8 @@ test.skip('makeBatches helper', t => {
 // TODO: make it all parallel!
 
 
-function setup() {
-  const d = process.env.LI_CACHE_PATH
-  if (fs.existsSync(d)) {
-    fs.rmdirSync(d, { recursive: true })
-  }
-  fs.mkdirSync(d)
-}
 
-function teardown() {
-  const d = process.env.LI_CACHE_PATH
-  if (fs.existsSync(d)) {
-    fs.rmdirSync(d, { recursive: true })
-  }
-}
 
-async function runTest(key, today) {
-  test(`${key} for ${today}`, async t => {
-    t.plan(3)
-    try {
-
-      const crawlArg = {
-        Records: [
-          { Sns: { Message: JSON.stringify({source: key}) } }
-        ]
-      }
-      console.log(`Calling scrape for ${key}`)
-      await crawlerHandler(crawlArg)
-      t.ok(`${key} crawl completed successfully.`)
-
-      const scrapeArg = {
-        Records: [
-          { Sns: { Message: JSON.stringify({source: key, date: today, silent: true}) } }
-        ]
-      }
-      const data = await scraperHandler(scrapeArg)
-      t.ok(`${key} scrape completed successfully.`)
-
-      // TODO (testing) verify that data matches a particular schema
-      // e.g.
-      //  const scraper = getScraper(key)
-      //  if (scraper.timeseries) {
-      //    checkMatchesTimeseriesSchema(t, data)
-      //  } else {
-      //    checkMatchesDataSchema(t, data)
-      //  }
-
-      // TODO (testing) verify that data was actually written.
-      t.ok(`${key} data written successfully.`)
-
-    } catch(err) {
-      t.fail(err)
-    }
-    finally {
-    }
-    t.end()
-  })
-}
-
-const today = datetime.today.utc()
-/*
-setup()
-for (const key of sourceKeys) {
-  runTest(key, today)
-}
-teardown()
-*/
-
-// ASYNC VERSION
 
 async function runFullCycle(key, today) {
 
@@ -165,26 +99,26 @@ async function runFullCycle(key, today) {
   }
 }
 
-function getInfoForEveryInnerArgument(keys, today) {
+function promiseRunFullCycleFor(keys, today) {
   const allruns = keys.map(k => runFullCycle(k, today))
   return Promise.all(allruns)
 }
 
 function mainFunction(batchedKeys, today) {
   return new Promise(function(resolve, reject) {
-    var results = []
+    var allResults = []
     var index = 0
     function next() {
       if (index < batchedKeys.length) {
-        const currBatch = batchedKeys[index]
-        console.log(`Running batch ${index + 1}: ${currBatch.join(',')}`)
-        getInfoForEveryInnerArgument(currBatch).then(function(data) {
-          results.push(data)
+        const batch = batchedKeys[index]
+        console.log(`Running batch ${index + 1}: ${batch.join(',')}`)
+        promiseRunFullCycleFor(batch, today).then(results => {
+          allResults = allResults.concat(results)
           next()
-        }, reject)
+        })
         index += 1
       } else {
-        resolve(results.flat())
+        resolve(allResults)
       }
     }
     // start first iteration
@@ -219,7 +153,26 @@ function testResults(results) {
   })
 }
 
-const batches = makeBatches(sourceKeys, 2)
+function setup() {
+  const d = process.env.LI_CACHE_PATH
+  if (fs.existsSync(d)) {
+    fs.rmdirSync(d, { recursive: true })
+  }
+  fs.mkdirSync(d)
+}
 
-mainFunction(batches, today).then(result => testResults(result))
+function teardown() {
+  const d = process.env.LI_CACHE_PATH
+  if (fs.existsSync(d)) {
+    fs.rmdirSync(d, { recursive: true })
+  }
+}
+
+const today = datetime.today.utc()
+const batches = makeBatches(sourceKeys, 10)
+
+setup()
+mainFunction(batches, today).
+  then(result => testResults(result))
+teardown()
 
