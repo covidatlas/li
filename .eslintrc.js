180c64156eea745c8ebc23c68c0716845b2c446e
Ryan Block <rblock@gmail.com>
2020-04-22 09:17:11 -0700

    Lint: no classes, prefer function declarations

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,16 +8,17 @@ module.exports = {
   },
   extends: 'eslint:recommended',
   plugins: [
+    'fp',
     'import'
   ],
   rules: {
     // Enable additional rules
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': [ 'error', 'unix' ],
     // Node specific
     'global-require': 'error',
     'handle-callback-err': 'error',
     // Override default options for rules from base configurations
-    'no-cond-assign': ['error', 'always'],
+    'no-cond-assign': [ 'error', 'always' ],
     // Disable rules from base configurations
     'arrow-body-style': 'off',
     'no-console': 'off',
@@ -27,11 +28,14 @@ module.exports = {
     // Style specific
     'no-trailing-spaces': 'error',
     // Cleanup on aisle semicolon
-    semi: [2, 'never'],
+    semi: [2, 'never' ],
+    // Functional
+    'fp/no-class': 'error',
     // Style
-    'space-before-function-paren': ['error', 'always'],
-    'object-curly-spacing': ['error', 'always'],
-    'array-bracket-spacing': ['error', 'always'],
+    'space-before-function-paren': [ 'error', 'always' ],
+    'object-curly-spacing': [ 'error', 'always' ],
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'func-style': [ 'error', 'declaration', { "allowArrowFunctions": true } ],
     // Plugins
     'import/no-unresolved': [2, {commonjs: true, amd: true}]
   },
