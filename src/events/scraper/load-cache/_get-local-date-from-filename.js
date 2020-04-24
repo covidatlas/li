5e6444f05645bfcf27a917e1b96d7eff9b2ddff5
Ryan Block <rblock@gmail.com>
2020-04-24 11:51:06 -0700

    hold

--- src/events/scraper/load-cache/_get-local-date-from-filename.js
+++ src/events/scraper/load-cache/_get-local-date-from-filename.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const convert = require('@architect/shared/cache/_convert-timestamp.js')
 const datetime = require('@architect/shared/datetime/index.js')
+const validate = require('@architect/shared/utils/validate-cache-filename.js')
 
 module.exports = function getLocalDateFromFilename (filename, tz='America/Los_Angeles') {
 
@@ -9,12 +10,8 @@ module.exports = function getLocalDateFromFilename (filename, tz='America/Los_An
   // Extract the file from the path
   file = path.basename(file)
 
-  // Sample filename: 2020-04-11t01_23_45.678z-default-117bb.html.gz
-  // TODO (cache-validation): this check belongs somewhere, but not here.
-  const check = /^\d{4}-\d{2}-\d{2}t\d{2}_\d{2}_\d{2}\.\d{3}[Zz]-[a-z]+-[a-h0-9]{5}\..*$/
-  if (!check.test(file)) {
-    throw new Error(`Bad cache filename ${filename}`)
-  }
+  // Throw now if it's invalid
+  validate(file)
 
   /**
    * Extract only the date, ignore the rest
--- tests/unit/events/scraper/load-cache/_get-local-date-from-filename-test.js
+++ tests/unit/events/scraper/load-cache/_get-local-date-from-filename-test.js
@@ -1,46 +1,51 @@
 const { join } = require('path')
 const test = require('tape')
 
-const sut = join(process.cwd(), 'src', 'events', 'scraper', 'load-cache', '_get-local-date-from-filename.js')
-const getLocalDateFromFilename = require(sut)
+const sut = join(process.cwd(), 'src', 'shared', 'utils', 'validate-cache-filename.js')
+const validate = require(sut)
 
 test('Date parsed correctly', t => {
-  const f = join('folder', 'subfolder', '2020-04-11t21_00_00.000z-default-117bb.html.gz')
-  const result = getLocalDateFromFilename(f, 'utc')
   t.plan(1)
-  t.equal('2020-04-11', result)
+  const f = '2020-04-11t21_00_00.000z-default-117bb.html.gz'
+  validate(f)
+  t.pass('We good')
 })
 
 test('Bad cache filename throws', t => {
-  const f = join('folder', 'subfolder', 'bad_filename')
   t.plan(1)
   try {
-    getLocalDateFromFilename(f, 'utc')
+    validate('bad_filename')
     t.fail('should have thrown')
   } catch (err) {
     t.equal(err.message, 'Bad cache filename folder/subfolder/bad_filename')
   }
 })
 
+test.only('Filenames with bad / missing extension throws', t => {
+  const badexts = [ '', 'html', 'UpperCase', '123' ]
+  t.plan(badexts.length)
+  badexts.forEach(n => {
+    const s = `2020-04-11t21_00_00.000z-default-117bb.${n}`
+    t.throws(() => validate(s), `${n} ext throws`)
+  })
+})
 
-// TODO (cache-validation): this test belongs somewhere, but not here.
 test('Filenames with bad keys throws', t => {
   const badnames = [ '', 'has space', 'UpperCase', '123' ]
   t.plan(badnames.length)
   badnames.forEach(n => {
-    const s = `2020-04-11t21_00_00.000z-{n}-117bb.html.gz`
+    const s = `2020-04-11t21_00_00.000z-${n}-117bb.html.gz`
     const f = join('folder', 'subfolder', s)
-    t.throws(() => getLocalDateFromFilename(f, 'utc'), `${n} name throws`)
+    t.throws(() => validate(f), `${n} name throws`)
   })
 })
 
-// TODO (cache-validation): this test belongs somewhere, but not here.
 test('Missing or bad sha throws', t => {
   const badshas = [ '', '12 34', 'UPPER', '123' ]
   t.plan(badshas.length)
   badshas.forEach(n => {
     const s = `2020-04-11t21_00_00.000z-default-${n}.html.gz`
     const f = join('folder', 'subfolder', s)
-    t.throws(() => getLocalDateFromFilename(f, 'utc'), `${n} sha throws`)
+    t.throws(() => validate(f), `${n} sha throws`)
   })
 })
