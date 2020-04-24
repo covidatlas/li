389ed299535d2e942dbbdb9fceb72401dd4e1133
Larry Davis <lazdnet@gmail.com>
2020-04-24 11:47:46 -0700

    Throw if no files provided instead of passing along undefined

--- src/events/scraper/load-cache/_get-date-bounds.js
+++ src/events/scraper/load-cache/_get-date-bounds.js
@@ -5,6 +5,10 @@ const getLocalDateFromFilename = require('./_get-local-date-from-filename.js')
  * Takes a cached filename, returns date boundaries
  */
 module.exports = function getDateBounds (files, tz) {
+  if (files.length === 0) {
+    throw new Error('No files provided')
+  }
+
   const sorted = sorter(files) // jic
   const earliest = getLocalDateFromFilename(sorted[0], tz)
   const latest = getLocalDateFromFilename(sorted[sorted.length - 1], tz)
