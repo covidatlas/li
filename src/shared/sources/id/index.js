e42cf8a5389ad06a3af2d36b45d203ba6165fce5
Cameron Chamberlain <git@camjc.com>
2020-04-30 20:58:47 +1000

    add ID source (Indonesia)

---
+++ src/shared/sources/id/index.js
@@ -0,0 +1,69 @@
+const assert = require('assert')
+const maintainers = require('../_lib/maintainers.js')
+const parse = require('../_lib/parse.js')
+
+const country = 'iso1:ID'
+
+const schemaKeysByHeadingFragment = {
+  sembuh: 'recovered',
+  meninggal: 'deaths',
+  'jumlah pdp': null, // pasien dalam pengawasan: "People in monitoring"
+  'jumlah odp': null, // orang dalam pemantauan: "Patients under supervision"
+  'positif covid-19': 'cases'
+}
+
+const pivotTable = arrayOfArrays => {
+  const newArray = []
+  arrayOfArrays.forEach((rowContent, rowIndex) => {
+    rowContent.forEach((columnContent, columnIndex) => {
+      newArray[columnIndex] = newArray[columnIndex] || []
+      newArray[columnIndex][rowIndex] = columnContent
+    })
+  })
+  return newArray
+}
+
+module.exports = {
+  aggregate: 'state',
+  country,
+  friendly: {
+    name: 'Ministry of Health Republic of Indonesia',
+    url: 'https://www.kemkes.go.id/'
+  },
+  maintainers: [ maintainers.camjc ],
+  scrapers: [
+    {
+      startDate: '2020-02-23',
+      crawl: [
+        {
+          type: 'page',
+          data: 'table',
+          url: 'https://www.kemkes.go.id/'
+        }
+      ],
+      scrape ($, date, { getSchemaKeyFromHeading, normalizeTable }) {
+        const normalizedTable = pivotTable(normalizeTable({ $, tableSelector: '.covid-case-container table' }))
+
+        const headingRowIndex = 0
+        const dataKeysByColumnIndex = []
+        normalizedTable[headingRowIndex].forEach((heading, index) => {
+          dataKeysByColumnIndex[index] = getSchemaKeyFromHeading({
+            heading: heading.replace('(Positif COVID-19)', ''),
+            schemaKeysByHeadingFragment
+          })
+        })
+
+        const dataRow = normalizedTable[normalizedTable.length - 1]
+
+        const data = {}
+        dataRow.forEach((value, columnIndex) => {
+          const key = dataKeysByColumnIndex[columnIndex]
+          data[key] = parse.number(value.replace('.', ''))
+        })
+
+        assert(data.cases > 0, 'Cases are not reasonable')
+        return data
+      }
+    }
+  ]
+}
