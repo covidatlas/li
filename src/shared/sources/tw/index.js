5d6569781b446da10b762a2194a4790366878802
Cameron Chamberlain <git@camjc.com>
2020-05-04 18:29:17 +1000

    non timeseries version

--- src/shared/sources/tw/index.js
+++ src/shared/sources/tw/index.js
@@ -1,34 +1,20 @@
 const assert = require('assert')
 const maintainers = require('../_lib/maintainers.js')
-const datetime = require('../../datetime/index.js')
+const parse = require('../_lib/parse.js')
 
 const country = `iso1:TW`
 
-const dateKeyForTested = '通報日'
-const dateKeyForRecovered = '發病日'
-
-const cumulateObject = items =>
-  items.reduce((previous, item) => {
-    const newObject = { ...previous }
-    for (const [ key, value ] of Object.entries(item)) {
-      if (typeof value === 'string' && value.includes('/')) {
-        continue
-      }
-      if (!newObject[key]) {
-        newObject[key] = value
-        continue
-      }
-      newObject[key] += value
-    }
-    return newObject
-  }, {})
-
+const schemaKeysByHeading = {
+  送驗: 'tested',
+  排除: null, // Negative Tests
+  確診: 'cases',
+  死亡: 'deaths',
+  解除隔離: 'recovered',
+}
 
 module.exports = {
-  aggregate: 'state',
   country,
   priority: 1,
-  timeseries: true,
   friendly: {
     name: 'Taiwan CDC',
     url: 'https://cdc.gov.tw/'
@@ -41,29 +27,16 @@ module.exports = {
         {
           type: 'json',
           url: 'https://covid19dashboard.cdc.gov.tw/dash4',
-          name: 'tested'
         },
-        {
-          type: 'json',
-          url: 'https://covid19dashboard.cdc.gov.tw/dash5',
-          name: 'recovered'
-        }
       ],
-      scrape ({ tested, recovered }, date) {
-        const currentTestedItems = Object.values(tested).filter(
-          item => datetime.dateIsBeforeOrEqualTo(datetime.parse(item[dateKeyForTested]), date)
-        )
-        const reducedTestedData = cumulateObject(currentTestedItems)
-
-        const currentRecoveredItems = Object.values(recovered).filter(
-          item => datetime.dateIsBeforeOrEqualTo(datetime.parse(item[dateKeyForRecovered]), date)
-        )
-        const reducedRecoveredData = cumulateObject(currentRecoveredItems)
-        const data = {
-          recovered: reducedRecoveredData['境外移入'],
-          tested: reducedTestedData.Total
+      scrape ($) {
+        const data = {}
+        for (const [ heading, value ] of Object.entries($['0'])) {
+          const schemaKey = schemaKeysByHeading[heading]
+          if (schemaKey) {
+            data[schemaKey] = parse.number(value)
+          }
         }
-        console.table(data)
 
         assert(data.cases > 0, 'Cases are not reasonable')
         return data
