dfc566e31ef729d28bdfc57f11b1ede8ff576863
Cameron Chamberlain <git@camjc.com>
2020-05-05 23:49:45 +1000

    add MM source, abstract groupBy util

--- src/shared/sources/jp/index.js
+++ src/shared/sources/jp/index.js
@@ -3,21 +3,6 @@ const maintainers = require('../_lib/maintainers.js')
 const transform = require('../_lib/transform.js')
 const datetime = require('../../datetime/index.js')
 
-/**
- * Hand rolled version of _.groupBy
- *
- * @param {object[]} array
- * @param {(object) => string} func - Get the key to group by.
- * @returns {object} Where key is the result of the function, and the value is an array of the values that match.
- */
-const groupBy = (array, func) => {
-  return array.reduce((previousValue, currentValue) => {
-    const currentKey = func(currentValue)
-    const oldVersionOfCurrentKey = previousValue[currentKey] || []
-    return { ...previousValue, [currentKey]: [ ...oldVersionOfCurrentKey, currentValue ] }
-  }, {})
-}
-
 const country = 'iso1:JP'
 module.exports = {
   aggregate: 'state',
@@ -39,7 +24,7 @@ module.exports = {
             'https://services8.arcgis.com/JdxivnCyd1rvJTrY/arcgis/rest/services/v2_covid19_list_csv/FeatureServer/0/query?where=0%3D0&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson'
         }
       ],
-      scrape ($, date, { getIso2FromName }) {
+      scrape ($, date, { getIso2FromName, groupBy }) {
         assert($.features.length > 1, 'features are unreasonable')
         const attributes = $.features
           .map(({ attributes }) => attributes)
