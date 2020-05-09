12fcfe4c8bd00aabe87164125f3b7ca8a3251de5
Cameron Chamberlain <git@camjc.com>
2020-05-09 11:34:19 +1000

    add AU-NT source

---
+++ src/shared/sources/au/nt/index.js
@@ -0,0 +1,49 @@
+const assert = require('assert')
+const maintainers = require('../../_lib/maintainers.js')
+const parse = require('../../_lib/parse.js')
+
+const schemaKeysByHeadingFragment = {
+  'confirmed cases': 'cases',
+  'people recovered': 'recovered',
+  'tests conducted': 'tested'
+}
+
+module.exports = {
+  country: 'iso1:AU',
+  state: 'iso2:AU-NT',
+  priority: 2,
+  friendly: {
+    name: 'Northern Territory Government - Coronavirus site',
+    url: 'https://coronavirus.nt.gov.au'
+  },
+  maintainers: [ maintainers.camjc ],
+  scrapers: [
+    {
+      startDate: '2020-04-09',
+      crawl: [
+        {
+          type: 'page',
+          data: 'table',
+          url: 'https://coronavirus.nt.gov.au/'
+        }
+      ],
+      scrape ($, date, { getSchemaKeyFromHeading }) {
+        const $rows = $('.header-widget div span, .header-widget p')
+        const data = {}
+        $rows.each((index, row) => {
+          const $row = $(row)
+          const [ value, ...headingWords ] = $row.text().split(' ')
+          const heading = headingWords.join(' ')
+          const numberInLabel = heading.match(/\d/)
+          if (!numberInLabel) {
+            const key = getSchemaKeyFromHeading({ heading, schemaKeysByHeadingFragment })
+            data[key] = parse.number(value)
+          }
+        })
+        assert(data.cases > 0, 'Cases is not reasonable')
+        console.log(data)
+        return data
+      }
+    }
+  ]
+}
