650fbacf4bbd2fa3ccf7e370269bb026b4a5dc54
Cameron Chamberlain <git@camjc.com>
2020-05-13 20:07:58 +1000

    Add NG (Nigeria) source (#122)
    * add NG nigeria source
    
    * use ARCGIS instead
    
    * remove superflous filter

---
+++ src/shared/sources/ng/index.js
@@ -0,0 +1,52 @@
+const assert = require('assert')
+const maintainers = require('../_lib/maintainers.js')
+const transform = require('../_lib/transform.js')
+
+const country = 'iso1:NG'
+
+module.exports = {
+  aggregate: 'state',
+  country,
+  priority: 1,
+  friendly: {
+    name: 'Nigeria Center for Disease Control',
+    url: 'https://covid19.ncdc.gov.ng/'
+  },
+  maintainers: [ maintainers.camjc ],
+  scrapers: [
+    {
+      startDate: '2020-05-13',
+      crawl: [
+        {
+          type: 'json',
+          url:
+            'https://services5.arcgis.com/Y2O5QPjedp8vHACU/arcgis/rest/services/NgeriaCovid19/FeatureServer/0//query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token='
+        }
+      ],
+      scrape ($, date, { getIso2FromName }) {
+        assert($.features.length > 0, 'features are unreasonable')
+        const attributes = $.features
+          .map(({ attributes }) => attributes)
+
+        assert(attributes.length > 1, 'data fetch failed, no attributes')
+
+        const states = []
+        attributes.forEach((attribute) => {
+          states.push({
+            state: getIso2FromName({ country, name: attribute.NAME_1.replace('Nassarawa', 'Nasarawa') }),
+            active: attribute.Active_Cases,
+            cases: attribute.ConfCases,
+            deaths: attribute.Deaths,
+            recovered: attribute.Recovery
+          })
+        })
+
+        const summedData = transform.sumData(states)
+        states.push(summedData)
+
+        assert(summedData.cases > 0, 'Cases are not reasonable')
+        return states
+      }
+    }
+  ]
+}
