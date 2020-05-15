d44f18a0629bb4353cbb86d4fbe898fe202e7445
Cameron Chamberlain <git@camjc.com>
2020-05-15 18:11:51 +1000

    port BE (belgium) source. Still a little messy IMO (#138)

---
+++ src/shared/sources/be/index.js
@@ -0,0 +1,167 @@
+// const assert = require('assert')
+const maintainers = require('../_lib/maintainers.js')
+const parse = require('../_lib/parse.js')
+const datetime = require('../../datetime/index.js')
+const transform = require('../_lib/transform.js')
+const { UNASSIGNED } = require('../_lib/constants.js')
+
+const country = 'iso1:BE'
+
+module.exports = {
+  aggregate: 'state',
+  country,
+  timeseries: true,
+  priority: 1,
+  friendly: {
+    name: 'Sciensano',
+    url: 'https://www.sciensano.be/en'
+  },
+  maintainers: [ maintainers.qgolsteyn, maintainers.camjc ],
+  scrapers: [
+    {
+      startDate: '2020-03-01',
+      crawl: [
+        {
+          name: 'cases',
+          type: 'csv',
+          url: 'https://epistat.sciensano.be/Data/COVID19BE_CASES_AGESEX.csv'
+        },
+        {
+          name: 'deaths',
+          type: 'csv',
+          url: 'https://epistat.sciensano.be/Data/COVID19BE_MORT.csv'
+        },
+        {
+          name: 'hospitalized',
+          type: 'csv',
+          url: 'https://epistat.sciensano.be/Data/COVID19BE_HOSP.csv'
+        },
+        {
+          name: 'tested',
+          type: 'csv',
+          url: 'https://epistat.sciensano.be/Data/COVID19BE_tests.csv'
+        }
+      ],
+      scrape ({ cases, deaths, hospitalized, tested }, date, { getIso2FromName }) {
+        const getIso2BE = (name) => {
+          const isoOverrides = { // Non-unique gets mapped straight to ISO2
+            'Flanders': 'iso2:BE-VLG',
+          }
+
+          const nameOverrides = { // Name differences get mapped to the canonical names
+            'Antwerpen': 'Antwerp',
+            "OostVlaanderen": "East Flanders",
+            "VlaamsBrabant": "Flemish Brabant",
+            "WestVlaanderen": "West Flanders",
+            "Li�ge": "Liège",
+            "BrabantWallon": "Walloon Brabant",
+            "NA": UNASSIGNED,
+          }
+
+          return isoOverrides[name] || getIso2FromName({ country, name: nameOverrides[name] || name })
+        }
+
+        const dataByRegion = {}
+        const dataByProvince = {}
+        let nationalData = { tested: 0 }
+
+        for (const item of cases) {
+          if (item.DATE === 'NA' || datetime.dateIsBeforeOrEqualTo(item.DATE, date)) {
+            if (!dataByProvince[item.REGION]) {
+              dataByProvince[item.REGION] = {}
+            }
+            const regionData = dataByProvince[item.REGION]
+
+            if (!regionData[item.PROVINCE]) {
+              regionData[item.PROVINCE] = {}
+            }
+            const provinceData = regionData[item.PROVINCE]
+
+            provinceData.cases = parse.number(item.CASES) + (provinceData.cases || 0)
+          }
+        }
+
+        for (const item of deaths) {
+          if (item.DATE === 'NA' || datetime.dateIsBeforeOrEqualTo(item.DATE, date)) {
+            if (!dataByRegion[item.REGION]) {
+              dataByRegion[item.REGION] = {}
+            }
+            const regionData = dataByRegion[item.REGION]
+
+            regionData.deaths = parse.number(item.DEATHS) + (regionData.deaths || 0)
+          }
+        }
+
+        for (const item of hospitalized) {
+          if (item.DATE === 'NA' || datetime.dateIsBeforeOrEqualTo(item.DATE, date)) {
+            if (!dataByProvince[item.REGION]) {
+              dataByProvince[item.REGION] = {}
+            }
+            const regionData = dataByProvince[item.REGION]
+
+            if (!regionData[item.PROVINCE]) {
+              regionData[item.REGION] = {}
+            }
+            const provinceData = regionData[item.PROVINCE]
+
+            provinceData.hospitalized = parse.number(item.NEW_IN) + (provinceData.hospitalized || 0)
+            provinceData.discharged = parse.number(item.NEW_OUT) + (provinceData.discharged || 0)
+          }
+        }
+
+        for (const item of tested) {
+          if (item.DATE === 'NA' || datetime.dateIsBeforeOrEqualTo(item.DATE, date)) {
+            nationalData.tested += parse.number(item.TESTS)
+          }
+        }
+
+        const data = []
+
+        for (const reg of Object.keys(dataByProvince)) {
+          let regionData = {
+            state: getIso2BE(reg),
+            ...dataByRegion[reg]
+          }
+
+          if ([ 'Flanders', 'Wallonia' ].includes(reg)) {
+            const provinceData = []
+            for (const prov of Object.keys(dataByProvince[reg])) {
+              provinceData.push({
+                state: getIso2BE(reg),
+                county: getIso2BE(prov),
+                ...dataByProvince[reg][prov]
+              })
+            }
+
+            regionData = transform.sumData(provinceData, regionData)
+            data.push(...provinceData)
+            data.push(regionData)
+          } else if (reg === 'Brussels') {
+            regionData = {
+              ...dataByProvince[reg][reg],
+              ...regionData
+            }
+
+            // Brussels is both a region and a province. Add to both
+            data.push({
+              ...regionData,
+              county: getIso2BE(reg)
+            })
+            data.push(regionData)
+          } else if (reg === 'NA') {
+            // Simply add this to the country total
+            regionData = {
+              ...dataByProvince[reg][reg],
+              ...regionData
+            }
+          }
+          nationalData = transform.sumData([ regionData ], nationalData)
+        }
+
+        data.push(nationalData)
+
+        return data
+      }
+    }
+  ]
+}
