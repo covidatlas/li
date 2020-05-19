50e0f76f8dc7907991f31bb9981535d61cc43e11
Jeff Zohrab <jzohrab@gmail.com>
2020-05-18 23:14:51 -0400

    TODO comments for us-ak parse fixes.

--- src/shared/sources/us/ak/index.js
+++ src/shared/sources/us/ak/index.js
@@ -4,7 +4,7 @@ const srcShared = '../../../'
 const maintainers = require(srcShared + 'sources/_lib/maintainers.js')
 const parse = require(srcShared + 'sources/_lib/parse.js')
 const transform = require(srcShared + 'sources/_lib/transform.js')
-
+const assert = require('assert')
 
 const regions = {
   Anchorage: [ 'Anchorage Municipality' ],
@@ -62,11 +62,14 @@ module.exports = {
         },
       ],
       scrape ($) {
-
         const counties = []
         const $table = $('td:contains("Seward")').closest('table')
+        console.log('table length = ')
+        console.log($table.length)
         const $trs = $table.find('tbody > tr')
+        let foundTrs = false
         $trs.each((index, tr) => {
+          foundTrs = true
           const $tr = $(tr)
           const cases = parse.number($tr.find('td:last-child').text())
           let region = parse.string($tr.find('> *:first-child').text())
@@ -88,10 +91,40 @@ module.exports = {
           }
           counties.push(countyObj)
         })
+        assert(foundTrs, 'found Trs')
         counties.push(transform.sumData(counties))
-        return counties
 
+        // TODO (scraper) the parsing here does not work, needs fixing or replacing.
+        // The 'county' in the returned data is an array, which doesn't work when it
+        // comes to normalizing and determining fips codes.
+        //
+        // The parsing creates data that looks like this:
+        // { "name":"Anchorage Economic Region, AK, USA",
+        //   "county":["Anchorage Municipality"], "cases":67}
+        // and Li throws an error when trying to process it:
+        //
+        // TypeError: county.startsWith is not a function
+        //    at lookupFIPS ( li/src/events/scraper/normalize-data/_normalize-us.js:40:27)
+        //    at li/src/events/scraper/normalize-data/index.js:20:16
+        console.log(JSON.stringify(counties))
+
+        return counties
       }
     }
+
+    // TODO (scraper): scrape new arcgis data
+
+    // Timeseries of data - at the state level only.  This should go in its own source.
+    // https://coronavirus-response-alaska-dhss.hub.arcgis.com/datasets/ ...
+    //     daily-cases-hospitalizations-and-deaths
+
+    // https://coronavirus-response-alaska-dhss.hub.arcgis.com/datasets/ ...
+    //     geographic-distribution-of-confirmed-cases
+    //     geographical-distribution-of-tests
+
+    // The arcgis data appears to use the same region names as above,
+    // but some of these datasets are broken down by the smaller
+    // regions.
+    // fips data: https://en.wikipedia.org/wiki/List_of_boroughs_and_census_areas_in_Alaska
   ]
 }
