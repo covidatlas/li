db717af828caa09b5135809ed5a7dd371aa8b909
JZ <jzohrab@gmail.com>
2020-05-21 09:35:25 -0400

    Port smc (#184)
    * Port us/ca/san-mateo-county.js
    * TODO comment.

---
+++ src/shared/sources/us/ca/san-mateo-county.js
@@ -0,0 +1,46 @@
+// Migrated from coronadatascraper, src/shared/scrapers/US/CA/san-mateo-county.js
+
+const srcShared = '../../../'
+const maintainers = require(srcShared + 'sources/_lib/maintainers.js')
+const parse = require(srcShared + 'sources/_lib/parse.js')
+
+module.exports = {
+  county: 'fips:06081',
+  state: 'iso2:US-CA',
+  country: 'iso1:US',
+  maintainers: [ maintainers.jbencina ],
+  scrapers: [
+    {
+      startDate: '2020-03-13',
+      crawl: [
+        {
+          type: 'page',
+          url: 'https://www.smchealth.org/coronavirus',
+        },
+      ],
+      scrape ($) {
+        // TODO (scrapers) Validate structure.
+        let deaths
+        let cases
+        const $th = $('th:contains("COVID-19 Case Count")')
+        const $table = $th.closest('table')
+        {
+          const $tr = $table.find('*:contains("Positive")').closest('tr')
+          const $dataTd = $tr.find('td:last-child')
+          cases = parse.number($dataTd.text())
+        }
+        {
+          const $tr = $table.find('*:contains("Deaths")').closest('tr')
+          const $dataTd = $tr.find('td:last-child')
+          deaths = parse.number($dataTd.text())
+        }
+        return {
+          cases,
+          deaths
+        }
+      }
+      // TODO (scrapers): this stopped returning data as at '2020-03-31', new page?
+      // Not implementing now, only porting.
+    }
+  ]
+}
