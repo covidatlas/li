082971a1c344636a8ba5d4f3e740eaa617751460
Jeff Zohrab <jzohrab@gmail.com>
2020-05-23 22:49:20 -0400

    Added failing pagination crawl tests.

---
+++ tests/integration/events/crawler/paginated-json-test.js
@@ -0,0 +1,32 @@
+process.env.NODE_ENV = 'testing'
+
+const utils = require('../utils.js')
+const test = require('tape')
+const testCache = require('../../_lib/testcache.js')
+
+test('crawl saves paginated results to cache as separate files', async t => {
+  await utils.setup()
+
+  const page1 = {
+    date: 'Jan 1',
+    cases: 1,
+    nextUrl: 'http://localhost:5555/tests/fake-source-urls/paginated-json/page2.json'
+  }
+  const page2 = {
+    date: 'Jan 2',
+    cases: 2
+  }
+  utils.writeFakeSourceContent('paginated-json/page1.json', page1)
+  utils.writeFakeSourceContent('paginated-json/page2.json', page2)
+
+  t.equal(0, testCache.allFiles().length, 'No files in cache.')
+  await utils.crawl('fake')
+  t.equal(2, testCache.allFiles().length, 'have both files after crawl.')
+
+  await utils.teardown()
+  t.end()
+})
+
+// TODO tests
+// Files are named correctly
+// missing page = nothing is saved
---
+++ tests/integration/fake-sources/sources/paginated-json.js
@@ -0,0 +1,39 @@
+module.exports = {
+  country: 'iso1:US',
+  state: 'CA',
+  maintainers: [],
+  friendly: {
+    name: 'Canadian COVID Rolling Task Force',
+    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+  },
+  scrapers: [
+    {
+      startDate: '2020-03-01',
+      crawl: [
+        {
+          type: 'json',
+          paginated: async client => {
+            const result = []
+            let currentUrl = 'http://localhost:5555/tests/fake-source-urls/paginated-json/page1.json'
+            while (currentUrl) {
+              let { body } = await client( { url: body.nextUrl } )
+              body = JSON.parse(`body`)
+              result.push(body)
+              currentUrl = body.nextUrl
+            }
+            return result
+          }
+        }
+      ],
+      scrape (json) {
+        const result = []
+        for (let i = 0; i <= json.length; ++i) {
+          json[i].forEach(rec => {
+            result.push( { date: rec.date, cases: rec.cases, page: i } )
+          })
+        }
+        return result
+      }
+    }
+  ]
+}
