cea86a28f19091474c1add8ae4d6094225a57d27
Ryan Block <rblock@gmail.com>
2020-05-24 18:28:48 -0700

    Broken promises, man

--- src/scheduled/runner/fire-events/index.js
+++ src/scheduled/runner/fire-events/index.js
@@ -1,19 +1,31 @@
 const arc = require('@architect/functions')
 
 module.exports = async function fireEvents (task, sources) {
+  let counter = 0
+
   // The return of el cheapo queue
   let queue = 0
-  for (const source of sources) {
-    // Invoke the timeseries scraper many times
-    setTimeout(async () => {
-      await arc.events.publish({
-        name: task,
-        payload: {
-          source: source._sourceKey
-        }
-      })
-    }, queue)
-    queue += 1000
-  }
-  console.log(`Publishing ${sources.length} ${task} events`)
+  const events = sources.map(source => {
+    return new Promise ((resolve, reject) => {
+      setTimeout(() => {
+        arc.events.publish({
+          name: task,
+          payload: {
+            source: source._sourceKey
+          }
+        }, function done (err) {
+          if (err) return reject(err)
+          else {
+            counter++
+            return resolve()
+          }
+        })
+      }, queue)
+      queue += 1000
+    })
+  })
+  return Promise.all(events).then(() => {
+    console.log(`Published ${counter} of ${sources.length} ${task} events`)
+  })
+
 }
