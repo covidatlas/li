8272fbebfabad137d371a82f8b17b91eaf55ed7f
Jeff Zohrab <jzohrab@gmail.com>
2020-05-28 15:39:42 -0400

    Add test for corrupted file load.

--- tests/integration/events/scraper/multiple-urls-test.js
+++ tests/integration/events/scraper/multiple-urls-test.js
@@ -46,3 +46,28 @@ test('scrape fails if files missing', async t => {
   await utils.teardown()
   t.end()
 })
+
+test('scrape fails if cache files are corrupted', async t => {
+  await utils.setup()
+
+  utils.writeFakeSourceContent('multiple-urls/cases.json', { count: 111 })
+  utils.writeFakeSourceContent('multiple-urls/deaths.json', { count: 222 })
+  t.equal(0, testCache.allFiles().length, 'No files in cache.')
+  await utils.crawl('multiple-urls')
+  t.equal(2, testCache.allFiles().length, '2 files saved after crawl.')
+
+  const f = path.join(testCache.testingCache, testCache.allFiles()[0])
+  fs.unlinkSync(f)
+  fs.writeFileSync(f, 'This is not json and it should fail parsing.')
+
+  try {
+    await utils.scrape('multiple-urls')
+    t.fail('should have failed')
+  }
+  catch (err) {
+    t.pass('failed with error: ' + err)
+  }
+
+  await utils.teardown()
+  t.end()
+})
