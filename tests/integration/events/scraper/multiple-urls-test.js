a36bd0a22a576c8cfe132861d424584b3bc85921
Jeff Zohrab <jzohrab@gmail.com>
2020-05-28 13:25:57 -0400

    Test coverage for local cache misses.

--- tests/integration/events/scraper/multiple-urls-test.js
+++ tests/integration/events/scraper/multiple-urls-test.js
@@ -1,6 +1,8 @@
 process.env.NODE_ENV = 'testing'
 
 const test = require('tape')
+const fs = require('fs')
+const path = require('path')
 const utils = require('../utils.js')
 const testCache = require('../../_lib/testcache.js')
 
@@ -16,6 +18,30 @@ test('scrape of multiple files', async t => {
 
   const fullResult = await utils.scrape('multiple-urls')
   utils.validateResults(t, fullResult, [ { cases: 111, deaths: 222 } ])
+
+  await utils.teardown()
+  t.end()
+})
+
+test('scrape fails if files missing', async t => {
+  await utils.setup()
+
+  utils.writeFakeSourceContent('multiple-urls/cases.json', { count: 111 })
+  utils.writeFakeSourceContent('multiple-urls/deaths.json', { count: 222 })
+  t.equal(0, testCache.allFiles().length, 'No files in cache.')
+  await utils.crawl('multiple-urls')
+  t.equal(2, testCache.allFiles().length, '2 files saved after crawl.')
+
+  const f = testCache.allFiles()[0]
+  fs.unlinkSync(path.join(testCache.testingCache, f))
+
+  try {
+    await utils.scrape('multiple-urls')
+    t.fail('should have failed')
+  }
+  catch (err) {
+    t.match(err.message, /Could not load cache/)
+  }
 
   await utils.teardown()
   t.end()
