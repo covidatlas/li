a703c7af3fb08aba936b8fa860369274ece277c5
JZ <jzohrab@gmail.com>
2020-06-21 10:37:33 -0400

    Add error message to status. (#243)

--- src/events/status/index.js
+++ src/events/status/index.js
@@ -7,7 +7,7 @@ const log = require('./log/index.js')
  */
 async function updateStatus (params) {
   try {
-    const { source, event, status } = params
+    const { source, event, status, error } = params
     // Status can be one of: success, failed
 
     const timeLabel = `Status update: ${source} / ${event} / ${status}`
@@ -22,8 +22,12 @@ async function updateStatus (params) {
     let newStatus = Object.assign({}, lastStatus)
     newStatus.status = status
 
-    if (status === 'success')
+    if (status === 'success') {
       newStatus.last_success = new Date().toISOString()
+      newStatus.error = null
+    } else {
+      newStatus.error = error
+    }
 
     /**
      * Refresh consecutive
