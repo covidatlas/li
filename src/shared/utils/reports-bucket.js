36c1a81b65d6f9a5fdf4959f0e777486912583d8
JZ <jzohrab@gmail.com>
2020-06-29 11:27:49 -0400

    Staging bucket name. (#258)

--- src/shared/utils/reports-bucket.js
+++ src/shared/utils/reports-bucket.js
@@ -2,12 +2,10 @@
  * CloudFormation generated bucket names
  */
 module.exports = function getReportsBucket () {
-  // TODO (reports) need proper bucket names.
-  throw new Error('TODO need proper keys')
-  /*
   const isProduction = process.env.NODE_ENV === 'production'
-  return isProduction
-    ? 'liproduction-reportsbucket-TODO-GET-PROD-NAME'
-    : 'listaging-reportsbucket-TODO-GET-STAGING-NAME'
-  */
+  if (!isProduction)
+    return 'listaging-reportsbucket-1bjqfmfwopcdd'
+
+  // TODO (reports) need proper bucket names.
+  throw new Error('TODO need prod key in src/shared/utils/reports-bucket.js')
 }
