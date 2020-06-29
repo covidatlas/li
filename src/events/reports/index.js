60dbe6797de156969205770cc82825c23018f0d5
Jeff Zohrab <jzohrab@gmail.com>
2020-06-28 22:19:11 -0400

    Simplify generation.

--- src/events/reports/index.js
+++ src/events/reports/index.js
@@ -40,58 +40,44 @@ async function doGeneration (hsh) {
  */
 async function handleEvent (event) {
 
-  // The reports to generate depend on baseJson, but we need to define
-  // them and their generation routines beforehand so that we can set
-  // them all to pending.  We can't create them with an enclosure b/c
-  // we generate the base data and so reassign its variable.  Storing
-  // the data in this dict so that the genBaseJson call can store its
-  // value here, and then the subsequent generation steps can pull
-  // their data from here.
-  const sourceData = {
-    baseJson: null
-  }
+  // All reports depend on baseJson, it must be generated first.
+  let baseJson = null
 
   // Integration tests can override where sources' code resides.
   const { _sourcesPath } = event
 
-  const genBaseJson = {
-    filename: 'baseData.json',
-    generate: async function () {
-      const base = await generateData.buildBaseJson( { _sourcesPath } )
-      sourceData.baseJson = base
-      return base
-    },
-    skipSave: true
-  }
-
   const reports = [
     {
+      filename: 'baseData.json',
+      generate: async function () {
+        baseJson = await generateData.buildBaseJson( { _sourcesPath } )
+      },
+      skipSave: true
+    },
+    {
       filename: 'locations.json',
-      generate: () => generateData.locations(sourceData.baseJson)
+      generate: () => generateData.locations(baseJson)
     },
     {
       filename: 'timeseries-byLocation.json',
-      generate: () => generateData.timeseriesByLocation(sourceData.baseJson)
+      generate: () => generateData.timeseriesByLocation(baseJson)
     },
     {
       filename: 'timeseries-jhu.csv',
-      generate: () => generateData.timeseriesJhu(sourceData.baseJson)
+      generate: () => generateData.timeseriesJhu(baseJson)
     },
     {
       filename: 'timeseries-tidy.csv',
-      generate: () => generateData.timeseriesTidy(sourceData.baseJson)
+      generate: () => generateData.timeseriesTidy(baseJson)
     },
     {
       filename: 'timeseries.csv',
-      generate: () => generateData.timeseries(sourceData.baseJson)
+      generate: () => generateData.timeseries(baseJson)
     }
   ]
 
-  // All are pending at the start
-  const rptNames = reports.concat(genBaseJson).map(r => r.filename)
-  await Promise.all(rptNames.map(n => reportStatus(n, 'pending')))
+  await Promise.all(reports.map(r => reportStatus(r.filename, 'pending')))
 
-  await doGeneration(genBaseJson)
   for (const r of reports) {
     await doGeneration(r)
   }
