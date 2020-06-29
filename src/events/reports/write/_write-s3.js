bfe0fa8e41801db54dbe255c90f347abb880eb61
Jeff Zohrab <jzohrab@gmail.com>
2020-06-28 22:53:22 -0400

    Write current and archive of report.

--- src/events/reports/write/_write-s3.js
+++ src/events/reports/write/_write-s3.js
@@ -6,15 +6,28 @@ module.exports = async function writeS3 (filename, data) {
 
   const Bucket = getReportsBucket()
 
+  const key = [ 'beta', 'latest', filename ].join('/')
+
   const params = {
     ACL: 'public-read',
     Bucket,
-    Key: filename,
+    Key: key,
     Body: data
   }
 
   const put = s3.putObject(params)
   await put.promise()
+
+  const archiveDate = new Date().toISOString().slice(0, 10)
+  const copyKey = [ 'beta', archiveDate, filename ].join('/')
+  console.log(`Copying /${key} to ${copyKey}`)
+  const copyParams = {
+    CopySource: `/${Bucket}/${key}`,
+    Bucket,
+    Key: copyKey
+  }
+  const s3copy = s3.copyObject(copyParams)
+  await s3copy.promise()
 
   console.log(`Wrote to S3: ${filename}`)
 }
