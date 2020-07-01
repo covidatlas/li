1ece5d77398c5513c8d389f7515d076f9c9556fc
Jeff Zohrab <jzohrab@gmail.com>
2020-07-01 11:50:27 -0400

    Add html view for report status.

--- src/http/get-report_status/index.js
+++ src/http/get-report_status/index.js
@@ -7,7 +7,71 @@ async function getJson () {
     then(items => items.sort((a, b) => a.reportSource < b.reportSource ? -1 : 1))
 }
 
-async function getReportStatus () {
+function toHtml (s) {
+  if (s === null || s === undefined || s === '')
+    return '&nbsp;'
+  return ('' + s).replace(/ /g, '&nbsp;').replace(/\n/g, '<br />')
+}
+
+/** Html page table of summary. */
+// TODO (status) generate HTML using some kind of templating engine.
+// Hacking to get HTML output for now, to be replaced w/ whatever
+// templating method we choose.
+function html (json) {
+
+  const shortDate = dt => (dt || '').replace('T', ' ').replace(/\..+/, '')
+  const tblJson = json.map(d => {
+    d.updated = shortDate(d.updated)
+    return d
+  })
+
+  const fields = [ 'report', 'status', 'updated' ]
+  const headings = fields
+  const ths = headings.map(h => `<th>${h}</th>`).join('')
+  const trs = tblJson.map(d => {
+    const tr = fields.map(f => `<td>${ toHtml(d[f]) }</td>`).join('')
+    return `<tr class='${d.status}'>${tr}</tr>`
+  })
+
+  return `
+<html>
+
+<style>
+p, h1 {
+  font-family: Arial;
+}
+
+th, td {
+  padding: 5px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
+
+tr:hover { background-color: #f5f5f5; }
+
+.success { color:green; }
+.generating { color: blue; }
+.pending { color: orange; }
+.failed { color:red; }
+</style>
+
+<body>
+<h1>Report Statuses</h1>
+
+<pre><code>
+<table>
+<tr>${ths}</tr>
+${trs.join('\n')}
+</table>
+
+</code></pre>
+</body>
+
+</html>`
+
+}
+
+async function getReportStatus (request) {
   const json = await getJson()
 
   // Default response is json.
@@ -17,6 +81,13 @@ async function getReportStatus () {
     headers: {
       'cache-control': 'max-age=300, s-maxage=300'
     }
+  }
+
+  // Html if requested.
+  const format = request.queryStringParameters['format'] || ''
+  if (format.toLowerCase() === 'html') {
+    result.body = html(json)
+    result.headers['Content-Type'] = 'text/html'
   }
 
   return result
