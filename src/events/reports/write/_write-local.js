595913a643d3490b3ff932bc4084b27b80ef1e60
Jeff Zohrab <jzohrab@gmail.com>
2020-07-05 12:21:42 -0400

    Clean up report code cruft from prior false starts.

--- src/events/reports/write/_write-local.js
+++ src/events/reports/write/_write-local.js
@@ -8,45 +8,6 @@ function write (params) {
   fs.writeFileSync(file, data)
 }
 
-
-// TODO (reports) delete everything after this.
-
-async function writeFile (filename, data) {
-  let reportPath = join(__dirname, '..', '..', '..', '..', 'reports')
-  // Alter the local cache dir (handy for integration testing)
-  if (process.env.LI_REPORT_PATH) {
-    reportPath = process.env.LI_REPORT_PATH
-  }
-  fs.mkdirSync(reportPath, { recursive: true })
-
-  const file = join(reportPath, filename)
-  fs.writeFileSync(file, data)
-  console.log(`Wrote report:`, file)
-}
-
-function getWritableStream (filename) {
-  let reportPath = join(__dirname, '..', '..', '..', '..', 'reports')
-  // Alter the local cache dir (handy for integration testing)
-  if (process.env.LI_REPORT_PATH) {
-    reportPath = process.env.LI_REPORT_PATH
-  }
-  fs.mkdirSync(reportPath, { recursive: true })
-
-  const file = join(reportPath, filename)
-  return {
-    writestream: fs.createWriteStream(file),
-    promise: Promise.resolve(true)
-  }
-}
-
-// eslint-disable-next-line no-unused-vars
-async function copyFileToArchive (filename) {
-  /* No-op for local */
-}
-
 module.exports = {
-  write,
-  writeFile,
-  getWritableStream,
-  copyFileToArchive
+  write
 }
--- src/events/reports/write/_write-s3.js
+++ src/events/reports/write/_write-s3.js
@@ -1,23 +1,18 @@
 const aws = require('aws-sdk')
-const stream = require('stream')
 const getReportsBucket = require('@architect/shared/utils/reports-bucket.js')
 
 async function write (params) {
   const { data, filename } = params
 
-  const s3 = new aws.S3()
-
   const Bucket = getReportsBucket()
-
   const key = [ 'beta', 'latest', filename ].join('/')
-
   const putParams = {
     ACL: 'public-read',
     Bucket,
     Key: key,
     Body: Buffer.from(data)
   }
-
+  const s3 = new aws.S3()
   const put = s3.putObject(putParams)
   await put.promise()
 
@@ -35,91 +30,6 @@ async function write (params) {
   console.log(`Wrote to S3: ${filename}`)
 }
 
-
-// TODO (reports) delete everything after this.
-
-async function writeFile (filename, data) {
-  const s3 = new aws.S3()
-
-  const Bucket = getReportsBucket()
-
-  const key = [ 'beta', 'latest', filename ].join('/')
-
-  const params = {
-    ACL: 'public-read',
-    Bucket,
-    Key: key,
-    Body: data
-  }
-
-  const put = s3.putObject(params)
-  await put.promise()
-
-  const archiveDate = new Date().toISOString().slice(0, 10)
-  const copyKey = [ 'beta', archiveDate, filename ].join('/')
-  console.log(`Copying /${key} to ${copyKey}`)
-  const copyParams = {
-    CopySource: `/${Bucket}/${key}`,
-    Bucket,
-    Key: copyKey
-  }
-  const s3copy = s3.copyObject(copyParams)
-  await s3copy.promise()
-
-  console.log(`Wrote to S3: ${filename}`)
-}
-
-
-function getWritableStream (filename) {
-  var pass = new stream.PassThrough()
-
-  const Bucket = getReportsBucket()
-
-  const Key = [ 'beta', 'latest', filename ].join('/')
-
-  const params = {
-    ACL: 'public-read',
-    Bucket,
-    Key,
-    Body: pass
-  }
-
-  const s3 = new aws.S3()
-  const promise = s3.upload(params, function (err, data) {
-    console.log(`Error: ${err}\nData: ${data}`)
-    throw err
-  }).promise()
-
-  return {
-    writestream: pass,
-    promise
-  }
-}
-
-
-async function copyFileToArchive (filename) {
-  const s3 = new aws.S3()
-
-  const Bucket = getReportsBucket()
-
-  const key = [ 'beta', 'latest', filename ].join('/')
-
-  const archiveDate = new Date().toISOString().slice(0, 10)
-  const copyKey = [ 'beta', archiveDate, filename ].join('/')
-  console.log(`Copying /${key} to ${copyKey}`)
-  const copyParams = {
-    CopySource: `/${Bucket}/${key}`,
-    Bucket,
-    Key: copyKey
-  }
-  const s3copy = s3.copyObject(copyParams)
-  await s3copy.promise()
-}
-
-
 module.exports = {
-  write,
-  writeFile,
-  getWritableStream,
-  copyFileToArchive
+  write
 }
