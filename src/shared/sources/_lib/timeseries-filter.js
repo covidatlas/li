60cff23fd49a62e2f27809af92e500c69316bc4c
Jeff Zohrab <jzohrab@gmail.com>
2020-07-07 23:18:16 -0400

    Add timeseries-filter helper.

---
+++ src/shared/sources/_lib/timeseries-filter.js
@@ -0,0 +1,54 @@
+/** Timeseries filtering.
+ *
+ * When scraping timeseries, we need to filter to only include data
+ * that matches the scrape date.  Sources report dates in slightly
+ * different formats, so we often need to convert the date to match
+ * the YYYY-MM-DD format of the 'date' string passed into
+ * `scrape(data, date, ...)`.
+ *
+ * In addition, many timeseries sources report data that is slightly
+ * stale (e.g., if running on 2020-06-20, the source may only include
+ * data up to 2020-06-19).  We therefore sometimes need to change the
+ * date that we're reporting on.
+ *
+ * Args:
+ *   data: array of hashes
+ *   dateField: field in data that contains date info
+ *   getYYYYMMDD: function to convert record[dateField] to YYYY-MM-DD format
+ *   date: the scrape date
+ *
+ * Returns
+ *   {
+ *     func: function to use as filter of data  (e.g. data.filter(func).map ... )
+ *     filterDate: the actual date of the filtered data returned.
+ *   }
+ *
+ * See test cases for more examples.
+ */
+module.exports = function timeseriesFilter (data, dateField, getYYYYMMDD, date) {
+  const result = {
+    func: () => true,
+    filterDate: date
+  }
+
+  if (data.length === 0)
+    return result
+
+  const allDates = [ ...data.map(d => d[dateField]) ].
+        map(dt => getYYYYMMDD(dt)).
+        sort()
+
+  if (date < allDates[0])
+    throw new Error(`Scrape date ${date} < earliest data data ${allDates[0]}`)
+
+  const latestDate = allDates.slice(-1)[0]
+
+  let filterDate = date
+  if (filterDate > latestDate)
+    filterDate = latestDate
+  console.log(`scraping data from ${filterDate}`)
+
+  const func = (d) => getYYYYMMDD(d[dateField]) === filterDate
+
+  return { func, filterDate }
+}
