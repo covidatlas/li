c7b5e31d5fd94d4abe40af182c3f177570f8f7ca
Jeff Zohrab <jzohrab@gmail.com>
2020-07-08 14:51:25 -0400

    Fix nl.

--- src/shared/sources/nl/index.js
+++ src/shared/sources/nl/index.js
@@ -2,6 +2,7 @@ const assert = require('assert')
 const maintainers = require('../_lib/maintainers.js')
 const parse = require('../_lib/parse.js')
 const transform = require('../_lib/transform.js')
+const timeseriesFilter = require('../_lib/timeseries-filter.js')
 
 const country = 'iso1:NL'
 
@@ -37,12 +38,14 @@ module.exports = {
         }
       ],
       scrape ({ provinces, national }, date, { getIso2FromName }) {
-        const casesData = provinces.filter(item => date === item[dateKey] && item[stateKey] && item[countKey])
-        assert(casesData.length > 0, `no cases data found for ${date}`)
+        const toYYYYMMDD = (datestring) => datestring
+        let { filterDate, func } = timeseriesFilter(provinces, dateKey, toYYYYMMDD, date)
+        const casesData = provinces.filter(func).filter(item => item[stateKey] && item[countKey])
+        assert(casesData.length > 0, `no cases data found for ${filterDate}`)
 
-        const hospitalized = national.find(item => date === item[dateKey] && item.Type === hospitalizedType)
-
-        const deaths = national.find(item => date === item[dateKey] && item.Type === deathsType)
+        const nfilt = timeseriesFilter(national, dateKey, toYYYYMMDD, date)
+        const hospitalized = national.filter(nfilt.func).find(item => item.Type === hospitalizedType)
+        const deaths = national.filter(nfilt.func).find(item => item.Type === deathsType)
 
         const data = []
 
@@ -51,17 +54,19 @@ module.exports = {
 
           data.push({
             state: getIso2FromName({ country, name }),
-            cases: parse.number(item[countKey])
+            cases: parse.number(item[countKey]),
+            date: filterDate
           })
         }
 
         data.push(
           transform.sumData(data, {
             hospitalized: hospitalized ? parse.number(hospitalized[countKey]) : undefined,
-            deaths: deaths ? parse.number(deaths[countKey]) : undefined
+            deaths: deaths ? parse.number(deaths[countKey]) : undefined,
+            date: filterDate
           })
         )
-        console.table(data)
+
         return data
       }
     }
