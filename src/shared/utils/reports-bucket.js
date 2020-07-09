84bce7a847bd45991dd1f6b7ab51ed0fc5a084bd
Jeff Zohrab <jzohrab@gmail.com>
2020-07-09 11:07:10 -0400

    Update prod reports bucket.

--- src/shared/utils/reports-bucket.js
+++ src/shared/utils/reports-bucket.js
@@ -5,7 +5,5 @@ module.exports = function getReportsBucket () {
   const isProduction = process.env.NODE_ENV === 'production'
   if (!isProduction)
     return 'listaging-reportsbucket-1bjqfmfwopcdd'
-
-  // TODO (reports) need proper bucket names.
-  throw new Error('TODO need prod key in src/shared/utils/reports-bucket.js')
+  return 'liproduction-reportsbucket-bhk8fnhv1s76'
 }
