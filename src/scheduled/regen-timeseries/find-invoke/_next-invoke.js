902352e1e697dc19720d78734790ad5ee29b97e2
Jeff Zohrab <jzohrab@gmail.com>
2020-07-10 00:18:13 -0400

    Passing test.

--- src/scheduled/regen-timeseries/find-invoke/_next-invoke.js
+++ src/scheduled/regen-timeseries/find-invoke/_next-invoke.js
@@ -2,6 +2,14 @@
 
 module.exports = function nextInvoke (invokes) {
 
+  const dateKey = a => `${a.lastInvoke || '1970-01-01T00:00:00.000Z'}/${a.key}`
+  const byDateThenKey = (a, b) => dateKey(a) < dateKey(b) ? - 1 : 1
+
+  /* sort invokes by date, mapping null dates to very old date so they
+   * go to the top. */
+  const invokesByStaleness = invokes.sort(byDateThenKey)
+
+  /*
   const next = invokes.find(i => {
     // Never regenerate more than every 12 hours
     const meow = new Date()
@@ -9,6 +17,8 @@ module.exports = function nextInvoke (invokes) {
     aBitAgo = new Date(aBitAgo).toISOString()
     return !i.lastInvoke || aBitAgo > i.lastInvoke
   })
+  */
+  const next = invokesByStaleness[0]
 
   return next.key
 }
