5b301bf984329ded26f372167ade76dcb0626293
Cameron Chamberlain <git@camjc.com>
2020-07-10 21:22:07 +1000

    [patch] fix CY source

--- src/shared/sources/cy/index.js
+++ src/shared/sources/cy/index.js
@@ -1,7 +1,7 @@
 const assert = require("assert")
 const maintainers = require("../_lib/maintainers.js")
 const timeseriesFilter = require("../_lib/timeseries-filter.js")
-const parse = require('../_lib/parse.js')
+const parse = require("../_lib/parse.js")
 
 const country = "iso1:CY"
 
@@ -20,34 +20,40 @@ module.exports = {
       startDate: "2020-03-09",
       crawl: [
         {
-          type: "json",
-          url:
-            "https://data.gov.cy/api/action/datastore/search.json?resource_id=611bd000-ce47-449b-b469-b4f50b960f61&limit=1000",
+          type: "csv",
+          url: "https://data.gov.cy/node/4618/download?language=en",
         },
       ],
       scrape ($, date) {
-
         // Sample date: '13/3/2020'
         function toYYYYMMDD (datestring) {
-          const [ d, m, y ] = datestring.split('/')
-          return [ y, m.padStart(2, '0'), d.padStart(2, '0') ].join('-')
+          const [ d, m, y ] = datestring.split("/")
+          return [ y, m.padStart(2, "0"), d.padStart(2, "0") ].join("-")
         }
 
-        const { filterDate, func } = timeseriesFilter($.result.records, 'date', toYYYYMMDD, date)
+        const { filterDate, func } = timeseriesFilter(
+          $,
+          "date",
+          toYYYYMMDD,
+          date
+        )
 
-        const data = $.result.records.filter(func).map(d => {
+        const data = $.filter(func).map((d) => {
           return {
             cases: parse.number(d["total cases"]),
             deaths: parse.number(d["total deaths"]),
             recovered: parse.number(d["total recovered"]),
             tested: parse.number(d["total tests"]),
-            date: filterDate
+            date: filterDate,
           }
         })
 
-        assert(data[0].cases > 0, "Cases are not reasonable for date: " + filterDate)
+        assert(
+          data[0].cases > 0,
+          "Cases are not reasonable for date: " + filterDate
+        )
         return data
-      }
-    }
-  ]
+      },
+    },
+  ],
 }
