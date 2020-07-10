c524abbcb327fde2f2cb98505f75483cec52dee2
Jeff Zohrab <jzohrab@gmail.com>
2020-07-10 11:07:17 -0400

    Final tests, get date range.

--- src/events/regenerator/fire-events/_get-date-range.js
+++ src/events/regenerator/fire-events/_get-date-range.js
@@ -1,11 +1,26 @@
 const datetime = require('@architect/shared/datetime/index.js')
 
+
+function throwIfNonDate (d) {
+  if (!d)
+    throw new Error('null or undefined date')
+  const dateRe  = /\d{4}-\d{2}-\d{2}/
+  if (!d.match(dateRe))
+    throw new Error(`date '${d}', does not match regex ${dateRe}`)
+}
+
 /** Gets an array of dates, with the earliest and the latest as the
  * boundaries.
  *
  * Such a simple function, but js can make it difficult.
  */
 module.exports = function getDatesRange (earliestYYYYMMDD, latestYYYYMMDD) {
+
+  throwIfNonDate(earliestYYYYMMDD)
+  throwIfNonDate(latestYYYYMMDD)
+
+  if (earliestYYYYMMDD > latestYYYYMMDD)
+    throw new Error(`${earliestYYYYMMDD} > ${latestYYYYMMDD}`)
 
   const d = new Date(earliestYYYYMMDD)
   const latest = new Date(latestYYYYMMDD)
@@ -15,5 +30,4 @@ module.exports = function getDatesRange (earliestYYYYMMDD, latestYYYYMMDD) {
     dates.push(datetime.getYYYYMMDD(d))
   }
   return dates
-
 }
--- tests/unit/events/regenerator/fire-events/_get-date-range-test.js
+++ tests/unit/events/regenerator/fire-events/_get-date-range-test.js
@@ -11,6 +11,14 @@ test('can get range', t => {
 })
 
 
+test('same start and end', t => {
+  const actual = getDatesRange('2020-07-11', '2020-07-11')
+  const expected = [ '2020-07-11' ]
+  t.deepEqual(expected, actual)
+  t.end()
+})
+
+
 test('works for daylight savings', t => {
   const actual = getDatesRange('2020-03-06', '2020-03-12')
   const expected = []
@@ -19,10 +27,69 @@ test('works for daylight savings', t => {
   t.end()
 })
 
-// daylight savings
-// month turnover
-// year turnover
-// leap year
-// throws if bad dates
-// throws if non-dates
-// throws if null
+
+test('works at month turnover', t => {
+  const actual = getDatesRange('2020-03-29', '2020-04-02')
+  const expected = [
+    '2020-03-29',
+    '2020-03-30',
+    '2020-03-31',
+    '2020-04-01',
+    '2020-04-02'
+  ]
+  t.deepEqual(expected, actual)
+  t.end()
+})
+
+
+test('works at year turnover', t => {
+  const actual = getDatesRange('2020-12-30', '2021-01-02')
+  const expected = [
+    '2020-12-30',
+    '2020-12-31',
+    '2021-01-01',
+    '2021-01-02'
+  ]
+  t.deepEqual(expected, actual)
+  t.end()
+})
+
+
+test('works at leap year', t => {
+  let actual = getDatesRange('2020-02-27', '2020-03-01')
+  let expected = [
+    '2020-02-27',
+    '2020-02-28',
+    '2020-02-29',
+    '2020-03-01'
+  ]
+  t.deepEqual(expected, actual, 'leap year')
+
+  actual = getDatesRange('2021-02-27', '2021-03-01')
+  expected = [
+    '2021-02-27',
+    '2021-02-28',
+    '2021-03-01'
+  ]
+  t.deepEqual(expected, actual, 'non-leap year')
+
+  t.end()
+})
+
+
+test('throws on non dates', t => {
+  t.throws(() => getDatesRange('something', 'here'))
+  t.end()
+})
+
+
+test('throws if earlier > later', t => {
+  t.throws(() => getDatesRange('2020-07-08', '2020-07-07'))
+  t.end()
+})
+
+
+test('throws if missing date', t => {
+  t.throws(() => getDatesRange(null, '2020-07-11'))
+  t.end()
+})
