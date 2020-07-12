2bea6e0d78eb2906fe1e3a7cdf7c6a31c7e4af25
Jeff Zohrab <jzohrab@gmail.com>
2020-07-12 15:52:24 -0400

    Add priority to source listing.

--- src/shared/sources/_lib/list-sources.js
+++ src/shared/sources/_lib/list-sources.js
@@ -3,20 +3,48 @@
 const { join, sep } = require('path')
 const sourceMap = require('./source-map.js')
 
-const m = sourceMap()
-
-const maxKeyLen = Math.max(...Object.keys(m).map(el => el.length))
 const baseDir = join(process.cwd(), 'src', 'shared', 'sources')
 
-function paddedKeyEntry (k) {
-  const keyPad = ' '.repeat(maxKeyLen - k.length + 1)
-  return k + keyPad
+const m = sourceMap()
+const data = Object.entries(m).
+      map(p => {
+        return {
+          key: p[0],
+          path: p[1],
+          shortPath: p[1].replace(`${baseDir}${sep}`, '')
+        }
+      }).
+      map(hsh => {
+        // eslint-disable-next-line
+        const s = require(hsh.path)
+        return {
+          ...hsh,
+          priority: s.priority || '',
+          maintainers: (s.maintainers || []).map(m => m.github).join(', ')
+        }
+      })
+
+
+const maxLengths = data.reduce((hsh, d) => {
+  Object.keys(d).forEach(k => {
+    if (hsh[k] === undefined)
+      hsh[k] = k.length
+    const sl = `${d[k]}`.length
+    if (sl > hsh[k]) {
+      console.log(`New entry ${d[k]} for key ${k}`)
+      hsh[k] = sl
+    }
+  })
+  return hsh
+}, {})
+// console.table(maxLengths)
+
+function printRow (d) {
+  const keys = [ 'key', 'priority', 'shortPath' ]
+  const out = keys.map(k => `${d[k]}`.padEnd(maxLengths[k] + 2, ' '))
+  console.log(out.join(''))
 }
 
-console.log(`${paddedKeyEntry('Source ID')} shared/sources/`)
-console.log(`${paddedKeyEntry('---------')} ---------------`)
-for (const [ k, p ] of Object.entries(m)) {
-  const kout = paddedKeyEntry(k)
-  const pout = p.replace(`${baseDir}${sep}`, '')
-  console.log(`${kout} ${pout}`)
-}
+printRow({ key: 'Source ID', shortPath: 'shared/sources', priority: 'priority' })
+printRow({ key: '---------', shortPath: '--------------', priority: '--------' })
+data.forEach(d => printRow(d))
