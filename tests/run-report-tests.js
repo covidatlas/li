52b691581e1b8eb6026569ff481d99053d82bef6
Jeff Zohrab <jzohrab@gmail.com>
2020-07-12 10:33:27 -0400

    Helper to run some tests.

---
+++ tests/run-report-tests.js
@@ -0,0 +1,5 @@
+// Run selected report tests.
+
+require('./unit/events/reports/generate-data/_build-timeseries-test.js')
+require('./unit/events/reports/generate-data/_multivalent-record-test.js')
+require('./integration/events/reports/generate-data/_build-base-json-test.js')
