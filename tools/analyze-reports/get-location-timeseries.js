60e3be1617daacb634b9a348085d833e910aa278
JZ <jzohrab@gmail.com>
2020-07-12 22:37:43 -0400

    Add baseData.json to downloads and some analysis. (#320)

--- tools/analyze-reports/get-location-timeseries.js
+++ tools/analyze-reports/get-location-timeseries.js
@@ -8,17 +8,24 @@ const path = require('path')
 
 
 function main (locationID) {
-  const downloadPath = path.join(__dirname, 'downloadedReports')
-  const fp = path.join(downloadPath, 'timeseries-byLocation.json')
-  const data = JSON.parse(fs.readFileSync(fp))
-  const loc = data.filter(rec => rec.locationID === locationID)
+  const files = [ 'timeseries-byLocation.json', 'baseData.json' ]
+  files.forEach(f => {
+    console.log('\n\n')
+    console.log('='.repeat(50))
+    console.log(f)
+    console.log()
+    const downloadPath = path.join(__dirname, 'downloadedReports')
+    const fp = path.join(downloadPath, f)
+    const data = JSON.parse(fs.readFileSync(fp))
+    const loc = data.filter(rec => rec.locationID === locationID)
 
-  if (loc.length === 0) {
-    console.log(`No match for locationID ${locationID}`)
-  }
-  else {
-    console.log(JSON.stringify(loc, null, 2))
-  }
+    if (loc.length === 0) {
+      console.log(`No match for locationID ${locationID}`)
+    }
+    else {
+      console.log(JSON.stringify(loc, null, 2))
+    }
+  })
 }
 
 
