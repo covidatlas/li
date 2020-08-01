678ee605e57b882eb91471f26e3cbbd277714277
JZ <jzohrab@gmail.com>
2020-07-31 21:30:50 -0400

    Rewrite and simplify jhu-usa. (#365)

--- src/shared/sources/jhu-usa.js
+++ src/shared/sources/jhu-usa.js
@@ -2,33 +2,50 @@
 
 const assert = require('assert')
 const srcShared = '../'
-const datetime = require(srcShared + 'datetime/index.js')
 const maintainers = require(srcShared + 'sources/_lib/maintainers.js')
 const parse = require(srcShared + 'sources/_lib/parse.js')
 const transform = require(srcShared + 'sources/_lib/transform.js')
 const { UNASSIGNED } = require(srcShared + 'sources/_lib/constants.js')
 
 
-/** Convert the date to a JHU-format m/d/yy, which they use for their
- * field names. */
-function getScrapeDateField (cases, date) {
-  // The data has a number of fields, some dates d/m/yy, some not.
-  const dates = Object.keys(cases[0]).filter(k => k.match(/^\d{1,2}\/\d{1,2}\/\d{2}$/))
+/**
+ * JHU data is CSV, with the headings containing non-dates, and then
+ * dates in m/d/y format
+ * (e.g. "...Combined_Key,Population,1/22/20,1/23/20...7/28/20,7/29/20").
+ * We need to pull out the data for the current scrape date, supplied
+ * as a string to the scrape() function. */
 
-  let scrapeDate = datetime.getYYYYMMDD(date)
-  const lastDate = datetime.getYYYYMMDD(new Date(`${dates[ dates.length - 1 ]} 12:00:00`))
-  if (scrapeDate > lastDate) {
-    const msg = `  🚨 ${scrapeDate} > last sample ${lastDate}. Using last sample`
-    console.error(msg)
-    scrapeDate = lastDate
-  }
-  const firstDate = datetime.getYYYYMMDD(new Date(`${dates[ 0 ]} 12:00:00`))
-  if (scrapeDate < firstDate) {
-    throw new Error(`Requested date ${scrapeDate} < earliest date ${firstDate}`)
-  }
+function mdyToYYYYMMDD (s) {
+  const [ m, d, y ] = s.split('/').map(n => parseInt(n, 10))
+  const ret = new Date(2000 + y, m - 1, d).toISOString().split('T')[0]
+  return ret
+}
 
-  const parts = scrapeDate.split('-').map(s => parseInt(s, 10))
+function YYYYMMDDtoMdy (s) {
+  const parts = s.split('-').map(s => parseInt(s, 10))
   return [ parts[1], parts[2], parts[0] - 2000 ].join('/')
+}
+
+
+function getScrapeDateYYYYMMDD (cases, date) {
+  // The data has a number of fields, some dates d/m/yy, some not.
+  const dates = Object.keys(cases[0]).
+        filter(k => k.match(/^\d{1,2}\/\d{1,2}\/\d{2}$/)).
+        map(mdyToYYYYMMDD).
+        sort()
+
+  if (date < dates[0]) {
+    throw new Error(`Requested date ${date} < earliest date ${dates[0]}`)
+  }
+
+  const lastDate = dates.slice(-1)[0]
+  console.log(`lastDate = ${lastDate}`)
+  if (date > lastDate) {
+    console.log(`Using date = ${lastDate}`)
+    return lastDate
+  }
+
+  return date
 }
 
 
@@ -64,79 +81,103 @@ module.exports = {
         },
       ],
       scrape ( { cases, deaths }, date, { fipsCodes } ) {
+
+        // First get all locationID parts for each row, starting from the
+        // country-levels data and working backwards.
+        // Return null if no locationID parts, else return struct.
+        function getLocationParts (caseInfo) {
+
+          let state = caseInfo.Province_State
+          let county = caseInfo.FIPS.replace(/\.0$/, '').padStart(5, '0')
+
+          if (county === '00000') {
+            county = null
+          }
+
+          // Disregard invalid county data (invalid, according to our country-levels data).
+          const disregardFIPS = [
+            'American Samoa',
+            'Guam',
+            'Northern Mariana Islands',
+            'Virgin Islands'
+          ]
+          if (disregardFIPS.includes(caseInfo.Province_State))
+            county = null
+
+          // Unassigned is always at the "county" level.
+          if (caseInfo.Admin2 === 'Unassigned') {
+            county = UNASSIGNED
+          }
+
+          const haveCounty = (county !== null && county !== UNASSIGNED)
+
+          if (haveCounty && !fipsCodes[county]) {
+            console.warn(`WARN: Skipping ${caseInfo.Combined_Key} at fips ${county} due to no fips data`)
+            return null
+          }
+
+          if (haveCounty) {
+            county = `fips:${county}`
+          }
+
+          if (caseInfo.code3 !== '840') {
+            // Guam, Puerto Rico, etc.  In country-levels, they are
+            // recorded as countries, and also as states, with
+            // optional additional fips data.
+            state = `iso2:US-${caseInfo.iso2}`
+          }
+
+          return { state, county }
+        }
+
+        for (const caseInfo of cases) {
+          caseInfo.locationParts = getLocationParts(caseInfo)
+        }
+
+        // Filter out bad locations.
+        const keepCases = cases.filter(caseInfo => caseInfo.locationParts !== null)
+        console.warn(`WARN: of ${cases.length} records, only importing ${keepCases.length}`)
+
+        const scrapeDate = getScrapeDateYYYYMMDD(keepCases, date)
+        const dateMDY = YYYYMMDDtoMdy(scrapeDate)
+
         let regions = []
-
-        const dateMDY = getScrapeDateField(cases, date)
-
-        const skippedNoFips = []
         const stateLocations = {}
-        for (let index = 0; index < cases.length; index++) {
-          // Get location info
-          const caseInfo = cases[index]
+        for (const caseInfo of keepCases) {
+          assert(caseInfo[dateMDY], `Have caseInfo for ${dateMDY}, keys = ${Object.keys(caseInfo)}`)
           const deathInfo = deaths.find(d => d.UID === caseInfo.UID)
           assert(deathInfo, `Have death UID ${caseInfo.UID}`)
 
-          const fips = caseInfo.FIPS.replace(/\.0$/, '').padStart(5, '0')
-          if ([ '00000', '88888', '99999' ].includes(fips)) {
-            console.warn('  ⚠️  Skipping incorrect FIPS code %s for %s', fips, caseInfo.Combined_Key)
-            continue
-          }
-
-          // Only include places we have data for.
-          // This is necessary, or Li throws when it tries to update locations.
-          const countryLevelIDInfo = fipsCodes[fips]
-          if (!countryLevelIDInfo) {
-            skippedNoFips.push(`${caseInfo.Combined_Key} at fips ${fips}`)
-            continue
-          }
-
-          if (caseInfo.Admin2.startsWith('Out of ')) {
-            console.warn('  ⚠️  Skipping out of state data for %s', caseInfo.Combined_Key)
-            continue
-          }
-
-          assert(caseInfo[dateMDY], `Have caseInfo for ${dateMDY}, keys = ${Object.keys(caseInfo)}`)
           const location = {
             cases: parse.number(caseInfo[dateMDY]),
-            deaths: parse.number(deathInfo[dateMDY])
-          }
-          // Puerto Rico, Guam, etc.
-          if (caseInfo.code3 !== '840') {
-            location.country = `iso1:${caseInfo.iso2}`
-            regions.push(location)
-          }
-          if (caseInfo.Admin2 === 'Unassigned') {
-            // const stateCode = geography.usStates[parse.string(caseInfo.Province_State)]
-            // const stateClid = `iso2:US-${stateCode}`
-            location.state = caseInfo.Province_State
-            location.county = UNASSIGNED
-            regions.push(location)
-            continue
+            deaths: parse.number(deathInfo[dateMDY]),
+            ...caseInfo.locationParts
           }
 
-          location.county = `fips:${fips}`
-          location.state = caseInfo.Province_State
+          regions.push(location)
+
           stateLocations[location.state] = stateLocations[location.state] || []
           stateLocations[location.state].push(location)
-          regions.push(location)
         }
-        // Sum the whole country
+
+        // Sum the whole country.
         regions.push(transform.sumData(regions))
-        // Sum individual states
+
+        // Sum individual states.  Note that single-entry locations
+        // such as Guam will be deduped when written to the db, as the
+        // locationID will be the same for both the individual entry
+        // and the "summed" entry.
         for (const [ state, locations ] of Object.entries(stateLocations)) {
           regions.push(transform.sumData(locations, { state }))
         }
+
         // remove unassigned counties once we have summed them up
         regions = regions.filter(r => r.county !== UNASSIGNED)
         if (regions.length === 0) {
           throw new Error(`Timeseries does not contain a sample for ${dateMDY}`)
         }
 
-        if (skippedNoFips.length > 0) {
-          console.warn(`⚠️ Skipping ${skippedNoFips.length} locations due to no fips`)
-          skippedNoFips.forEach(s => { console.warn(`  ${s}`) })
-        }
-        return regions
+        return regions.map(r => Object.assign(r, { date: scrapeDate }))
       }
     }
   ]
