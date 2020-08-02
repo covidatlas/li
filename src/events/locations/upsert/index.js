a8c8c052c3605f5ad1d6f7d9433d91c1cbd7fab8
Jeff Zohrab <jzohrab@gmail.com>
2020-08-02 15:08:06 -0400

    Issue 356: fix location load.
    Increase timeout.  jhu-usa creates thousands of locations, needs
    40 s to complete.  Have simplified code to remove a query, halving
    the lambda time.

--- src/events/locations/upsert/index.js
+++ src/events/locations/upsert/index.js
@@ -2,26 +2,8 @@ const arc = require('@architect/functions')
 
 module.exports = async function upsertLocations (locations) {
   const data = await arc.tables()
-
-  // Iterate and upsert locations
   for (const location of locations) {
-    const { slug, locationID } = location
-    const now = new Date().toISOString()
-
-    // Determine whether this location exists yet
-    const loc = await data.locations.get({ slug })
-
-    // Create location if not
-    if (!loc) {
-      console.log(`Creating new location: ${slug} / ${locationID}`)
-      location.created = now
-      await data.locations.put(location)
-    }
-    // Update the, uh, 'updated' timestamp
-    else {
-      location.created = loc.created
-      location.updated = now
-      await data.locations.put(location)
-    }
+    location.updated = new Date().toISOString()
+    await data.locations.put(location)
   }
 }
