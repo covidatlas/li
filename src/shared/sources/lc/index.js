c466b52d9961bbeb420005406d1057ddc72c97bd
Jeff Zohrab <jzohrab@gmail.com>
2020-08-03 21:55:27 -0400

    Fix St. Lucia.

--- src/shared/sources/lc/index.js
+++ src/shared/sources/lc/index.js
@@ -33,6 +33,38 @@ module.exports = {
 
         return data
       }
+    },
+    {
+      startDate: '2020-07-11',
+      crawl: [
+        {
+          type: 'page',
+          url: 'https://www.covid19response.lc/',
+          data: 'paragraph'
+        },
+      ],
+      scrape ($) {
+        const clean = s => s.trim().replace(/\n/g, '').replace(/ +/g, ' ')
+        const stats = $('div.wrapper').
+              toArray().
+              map(w => $(w)).
+              map(w => [ clean(w.find('div').eq(0).text()), clean(w.find('div.yellow').text()) ]).
+              reduce((hsh, pair) => {
+                hsh[pair[0]] = parseInt(pair[1].replace(/,/g, ''), 10)
+                return hsh
+              }, {})
+
+        const data = {
+          cases: stats['Number of Confirmed Cases of COVID-19 in Saint Lucia'],
+          deaths: stats['Number of COVID-19 Deaths in Saint Lucia'],
+          tested: stats['Number of COVID-19 Tests done in Saint Lucia'],
+          recovered: stats['Number of Persons Recovered from COVID-19']
+        }
+
+        assert(data.cases > 0, 'Cases are not reasonable')
+
+        return data
+      }
     }
   ]
 
