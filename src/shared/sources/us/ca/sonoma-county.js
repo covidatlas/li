d98360bdac5e954b2a5816082863c7e7c57ad038
Minh Nguyễn <mxn@1ec5.org>
2020-08-09 06:48:46 -0700

    Modernized Sonoma County, California (#381)

---
+++ src/shared/sources/us/ca/sonoma-county.js
@@ -0,0 +1,55 @@
+const maintainers = require('../../_lib/maintainers.js')
+const arcgis = require('../../_lib/arcgis.js')
+const timeseriesFilter = require('../../_lib/timeseries-filter.js')
+
+module.exports = {
+  country: 'iso1:US',
+  state: 'iso2:US-CA',
+  county: 'fips:06097',
+
+  maintainers: [ maintainers.camjc, maintainers.mnguyen ],
+
+  timeseries: true,
+  priority: 2,
+  friendly: {
+    name: 'Sonoma County Department of Emergency Management',
+    url: 'https://socoemergency.org/emergency/novel-coronavirus/coronavirus-cases/'
+  },
+
+  scrapers: [
+    {
+      startDate: '2020-03-02',
+      crawl: [
+        {
+          type: 'json',
+          paginated: arcgis.paginated('https://services1.arcgis.com/P5Mv5GY5S66M8Z1Q/ArcGIS/rest/services/NCOV_Cases_Sonoma_County/FeatureServer/0/query'),
+        },
+      ],
+      scrape (data, date) {
+        const toYYYYMMDD = t => {
+          const date = new Date(t)
+          // Convert from UTC to PDT
+          date.setUTCHours(-7)
+          return date.toISOString().split('T')[0]
+        }
+
+        const { filterDate, func } = timeseriesFilter(data, 'Date', toYYYYMMDD, date)
+        const filteredData = data.filter(func)
+        const result = {
+          cases: filteredData[0].Cumulative,
+          active: filteredData[0].Active,
+          deaths: filteredData[0].Deaths,
+          recovered: filteredData[0].Recovered,
+          date: filterDate,
+        }
+
+        if (result.cases === null && result.deaths === null) {
+          throw new Error(`No data as at ${date}`)
+        }
+
+        return result
+      }
+    },
+  ]
+
+}
