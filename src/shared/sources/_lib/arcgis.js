b46a666e290efe5fea1fb6c2eba44bee92d63151
Jeff Zohrab <jzohrab@gmail.com>
2020-08-09 09:42:37 -0400

    Reorg code

--- src/shared/sources/_lib/arcgis.js
+++ src/shared/sources/_lib/arcgis.js
@@ -1,47 +1,30 @@
 const querystring = require('querystring')
 
-/**
- * Open the arcgis iframe and look in the Network/XHR tab for requests with Name of "0".
- *
- * Like this one:
- * https://services7.arcgis.com/4RQmZZ0yaZkGR1zy/arcgis/rest/services/COVID19_testsites_READ_ONLY/FeatureServer/0?f=json
- *
- * serverNumber is 7, from services7.arcgis.com
- * orgId is 4RQmZZ0yaZkGR1zy
- * layerName is COVID19_testsites_READ_ONLY
- */
-async function urlFromOrgId (client, serverNumber, orgId, layerName) {
-  const url = `https://services${serverNumber}.arcgis.com/${orgId}/arcgis/rest/services/${layerName}/FeatureServer/0?f=json`
-  let { body } = await client( { url } )
-  body = JSON.parse(`[${body}]`)
-  const { serviceItemId } = body[0]
-  let ret = `https://opendata.arcgis.com/datasets/${serviceItemId}_0.csv`
-  console.log(`Calling ${ret}`)
-  return ret
-}
 
 /**
- * Get the URL for the CSV data from an ArcGIS dashboard
+ * Retrieves data from an ArcGIS REST API.  Default retrieves all
+ * items at the provided link with geometry turned off.
  *
- * @param {*} client the client object
- * @param {*} serverNumber the servern number, find this by looking at requests (i.e. https://services1.arcgis.com/ is serverNumber = 1)
- * @param {*} dashboardId the ID of the dashboard, as passed to the iframe that renders it (i.e. https://maps.arcgis.com/apps/opsdashboard/index.html#/ec4bffd48f7e495182226eee7962b422 is dashboardId = ec4bffd48f7e495182226eee7962b422)
- * @param {*} layerName the name of the layer to fetch data for, find this by examining requests
- */
-async function csvUrl (client, serverNumber, dashboardId, layerName) {
-  const dashboardUrl = `https://maps.arcgis.com/sharing/rest/content/items/${dashboardId}?f=json`
-  let { body } = await client( { url: dashboardUrl } )
-  const { orgId } = JSON.parse(body)
-  return urlFromOrgId(client, serverNumber, orgId, layerName)
-}
-
-
-/**
- * Retrieves data from an ArcGIS REST API. By default, it will
- * retrieve all items at the provided linked with geometry turned off.
- * You can control pagination size through the `featuresToFetch`
- * parameter in `options`, but this should not be necessary - by
- * default, this will make the largest request allowed by the source.
+ * You can pass in options that correspond to the fields in the http
+ * form of the feature layer query URL.  This lets you do things such
+ * as calculate record counts, maxes and mins, etc, using the query
+ * and host server, rather than doing complex calculations in scrape
+ * functions.
+ *
+ * Example usage:
+ *
+ * crawl: [
+ *   {
+ *     type: 'json',
+ *     paginated: arcgis.paginated(
+ *       'https://services6.arcgis.com/Bd4MACzvEukoZ9mR/arcgis/rest/services/Daily_COVID19_Testing_Report_for_OPI/FeatureServer/0/query',
+ *       {
+ *         groupByFieldsForStatistics: 'county,result,test_date',
+ *         outStatistics: '[{"statisticType":"count","onStatisticField":"*","outStatisticFieldName":"Count"}]'
+ *       }
+ *     )
+ *   }
+ * ],
  *
  * @param {string} featureLayerURL URL of the resource, up to and
  * including the feature layer number and `query`, e.g.
@@ -114,6 +97,43 @@ function paginated (featureLayerURL, options = {}) {
       return json.features.map(f => f.attributes)
     }
   }
+}
+
+
+/**
+ * Get the URL for the CSV data from an ArcGIS dashboard
+ *
+ * @param {*} client the client object
+ * @param {*} serverNumber the servern number, find this by looking at requests (i.e. https://services1.arcgis.com/ is serverNumber = 1)
+ * @param {*} dashboardId the ID of the dashboard, as passed to the iframe that renders it (i.e. https://maps.arcgis.com/apps/opsdashboard/index.html#/ec4bffd48f7e495182226eee7962b422 is dashboardId = ec4bffd48f7e495182226eee7962b422)
+ * @param {*} layerName the name of the layer to fetch data for, find this by examining requests
+ */
+async function csvUrl (client, serverNumber, dashboardId, layerName) {
+  const dashboardUrl = `https://maps.arcgis.com/sharing/rest/content/items/${dashboardId}?f=json`
+  let { body } = await client( { url: dashboardUrl } )
+  const { orgId } = JSON.parse(body)
+  return urlFromOrgId(client, serverNumber, orgId, layerName)
+}
+
+
+/**
+ * Open the arcgis iframe and look in the Network/XHR tab for requests with Name of "0".
+ *
+ * Like this one:
+ * https://services7.arcgis.com/4RQmZZ0yaZkGR1zy/arcgis/rest/services/COVID19_testsites_READ_ONLY/FeatureServer/0?f=json
+ *
+ * serverNumber is 7, from services7.arcgis.com
+ * orgId is 4RQmZZ0yaZkGR1zy
+ * layerName is COVID19_testsites_READ_ONLY
+ */
+async function urlFromOrgId (client, serverNumber, orgId, layerName) {
+  const url = `https://services${serverNumber}.arcgis.com/${orgId}/arcgis/rest/services/${layerName}/FeatureServer/0?f=json`
+  let { body } = await client( { url } )
+  body = JSON.parse(`[${body}]`)
+  const { serviceItemId } = body[0]
+  let ret = `https://opendata.arcgis.com/datasets/${serviceItemId}_0.csv`
+  console.log(`Calling ${ret}`)
+  return ret
 }
 
 
