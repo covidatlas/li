e947018258a7c4bb553673c40e672ada3876af61
Jeff Zohrab <jzohrab@gmail.com>
2020-08-15 20:28:45 -0400

    wip

--- src/scheduled/status-slack-bot/_generate-status-report.js
+++ src/scheduled/status-slack-bot/_generate-status-report.js
@@ -6,16 +6,6 @@
 const got = require('got')
 const site = require('@architect/shared/utils/site.js')
 
-const SINCE_LAUNCH = '(always)'
-
-function daysSinceLastSuccess (now, last_success_string) {
-  if (last_success_string === null)
-    return SINCE_LAUNCH
-  const last_success = new Date(last_success_string)
-  const diff = (now.getTime() - last_success.getTime())
-  const failure_for_days = Math.floor(diff / (1000 * 60 * 60 * 24))
-  return failure_for_days
-}
 
 /** Get the failures. */
 async function getScraperReport () {
@@ -24,33 +14,15 @@ async function getScraperReport () {
   const result = await got(path)
   const data = JSON.parse(result.body)
 
-  const now = new Date()
   const failures = data.details.
         filter(d => d.status !== 'success').
         map(d => {
-          const failType = (d.crawler_status !== 'success') ?
-                'crawler' : 'scraper'
-
-          const failing_for_days = daysSinceLastSuccess(now, d[`${failType}_last_success`])
-
+          const failType = (d.crawler_status !== 'success') ? 'crawler' : 'scraper'
           return {
             source: d.source,
             message: d[`${failType}_error`],
-            failing_for_days
+            last_success: d[`${failType}_last_success`]
           }
-        }).
-        sort((a, b) => {
-          let af = a.failing_for_days
-          if (af === SINCE_LAUNCH)
-            af = 10000
-          let bf = b.failing_for_days
-          if (bf === SINCE_LAUNCH)
-            bf = 10000
-          if (af === bf)
-            return 0
-          if (af > bf)
-            return -1
-          return 1
         })
 
   return {
@@ -59,8 +31,16 @@ async function getScraperReport () {
   }
 }
 
+
 /** Markdown table of failures. */
 function failureTable (failures) {
+
+  function daysSinceLastSuccess (now, last_success_string) {
+    const last_success = new Date(last_success_string)
+    const diff = (now.getTime() - last_success.getTime())
+    const failure_for_days = Math.floor(diff / (1000 * 60 * 60 * 24))
+    return failure_for_days
+  }
 
   function truncate (msg, truncateAt = 50) {
     let tmp = msg.split('\n')[0]
@@ -70,21 +50,19 @@ function failureTable (failures) {
     return `${tmp} ...`
   }
 
-  function makeRow (row) {
-    return [ `${row[0]}`.padStart(4, ' '), row[1] ].join('  ')
-  }
-
+  const now = new Date()
   const outrows = failures.
         map(f => {
           const msg = [ f.source, f.message ].join(': ')
-          return [ f.failing_for_days, truncate(msg, 70) ]
+          return [ daysSinceLastSuccess(now, f.last_success), truncate(msg, 70) ]
         })
+
   let table = []
   table.push([ 'Days', 'Failure' ])
   table.push([ '----', '-------' ])
   table = table.concat(outrows)
-  table = table.map(r => makeRow(r))
 
+  table = table.map(r => [ `${r[0]}`.padStart(4, ' '), r[1] ].join('  '))
   return [ '```', table.join('\n'), '```' ].join('')
 }
 
@@ -98,13 +76,21 @@ function getReportStruct (scraperReport) {
   const summary = scraperReport.summary
 
   const alwaysFailing = scraperReport.failures.
-        filter(f => f.failing_for_days === SINCE_LAUNCH).
+        filter(f => f.last_success === null).
         map(f => f.source)
   const allFailList = [ '```', alwaysFailing.join(', '), '```' ].join('')
   const alwaysFailingText = `${alwaysFailing.length} sources have never worked:
 ${allFailList}`
 
-  const failures = scraperReport.failures.filter(f => f.failing_for_days !== SINCE_LAUNCH)
+  const failures = scraperReport.failures.
+        filter(f => f.last_success !== null).
+        sort((a, b) => {
+          if (a.last_success === b.last_success)
+            return 0
+          if (a.last_success > b.last_success)
+            return 1
+          return -1
+        })
   let failuresText = `${failures.length} failures:
 ${failureTable(failures)}`
 
